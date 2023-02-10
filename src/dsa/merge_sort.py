5054a45232b7f8add4f3ba69fc613a3364f4e913
Mario <mario.nunez.callejo@gmail.com>
2023-02-10 19:16:47 +0100

    Implement merge sort algorithm

---
+++ src/dsa/merge_sort.py
@@ -0,0 +1,58 @@
+"""Implementation of merge sort algorithm"""
+
+
+import operator
+
+
+OPS = {
+    ">": operator.gt,
+    "<": operator.lt,
+}
+
+
+def merge(lst1, lst2, reverse):
+    """Merge two sorted list into one single sorted list"""
+    combined = []
+    i, j = 0, 0
+    if reverse is True:
+        op_order = ">"
+    else:
+        op_order = "<"
+
+    # stop loop when one of both lists does not have any items left
+    while i < len(lst1) and j < len(lst2):
+        if OPS[op_order](lst1[i], lst2[j]):
+            combined.append(lst1[i])
+            i += 1
+        else:
+            combined.append(lst2[j])
+            j += 1
+    # add the remaining items
+    while i < len(lst1):
+        combined.append(lst1[i])
+        i += 1
+    while j < len(lst2):
+        combined.append(lst2[j])
+        j += 1
+    return combined
+
+def merge_sort(lst, reverse=False):
+    """Sort a list using merge sort algorithm"""
+    # base case
+    if len(lst) == 1:
+        return lst
+    mid_index = int(len(lst)/2)
+    left = merge_sort(lst[:mid_index], reverse)
+    right = merge_sort(lst[mid_index:], reverse)
+    return merge(left, right, reverse)
+
+
+if __name__ == '__main__':
+    
+    print('\n##### TEST MERGE SORT #####')
+    list = [4, 2, 6, 5, 1, 3, 0, 37, 15, 23, 5, 4]
+    print('Before list sort:', list)
+    sorted_list = merge_sort(list)
+    print('AFTER asc sort:  ', sorted_list)
+    sorted_list = merge_sort(list, True)
+    print('AFTER desc sort: ', sorted_list)
