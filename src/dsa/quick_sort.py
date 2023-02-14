64d74e2b55c66fef92ac2292a6922f6911f91a01
Mario <mario.nunez.callejo@gmail.com>
2023-02-14 19:32:22 +0100

    Implement quick sort algorithm

---
+++ src/dsa/quick_sort.py
@@ -0,0 +1,42 @@
+"""Implementation of quick sort algorithm"""
+
+
+def swap(lst, idx1, idx2):
+    """Swap items within the list"""
+    temp = lst[idx1]
+    lst[idx1] = lst[idx2]
+    lst[idx2] = temp
+
+def pivot(lst, pivot_idx, end_idx):
+    """Separate the list in greater and less than a pivot point, sort the pivot
+    point and return the index of the sorted pivot point"""
+    swap_idx = pivot_idx
+    # Separate the list in greater and less than a pivot point
+    for i in range(pivot_idx+1, end_idx+1):
+        if lst[i] < lst[pivot_idx]:
+            swap_idx += 1
+            swap(lst, swap_idx, i)
+    # sort the pivot point
+    swap(lst, pivot_idx, swap_idx)
+    return swap_idx
+
+def quick_sort_helper(lst, first_idx, last_idx):
+    """Implement quick sort algorithm"""
+    # base case: left index < right index
+    if first_idx < last_idx:
+        pivot_idx = pivot(lst, first_idx, last_idx)
+        quick_sort_helper(lst, first_idx, pivot_idx-1)
+        quick_sort_helper(lst, pivot_idx+1, last_idx)
+    return lst
+
+def quick_sort(lst):
+    """Sort a list using quick sort algorithm"""
+    return quick_sort_helper(lst, 0, len(lst)-1)
+
+
+if __name__ == '__main__':
+    print('\n##### TEST QUICK SORT #####')
+    list = [4, 2, 6, 5, 1, 3, 0, 37, 15, 23, 5, 4]
+    print('Before list sort:', list)
+    sorted_list = quick_sort(list)
+    print('AFTER asc sort:  ', sorted_list)
